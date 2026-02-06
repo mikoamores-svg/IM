7059704bf37a03163c764aafc89e4eab98132e93
Miko John Amores <mikoamores@gmail.com>
2026-02-06 20:45:58 +0800

    commit

--- connections.php
+++ connections.php
@@ -3,4 +3,12 @@ $connections = mysqli_connect("localhost", "root", "", "gerbag_db");
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+
+?>
+
+<?php
+$conn = new mysqli("localhost", "root", "", "gerbag_db");
+if ($conn->connect_error) {
+    die("DB connection failed: " . $conn->connect_error);
+}
 ?>
